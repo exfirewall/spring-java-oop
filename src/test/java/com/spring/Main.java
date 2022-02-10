5c3379d59a9564481feb6f2bf95bb13936261ad7
jaehoon_choi <jaehoon_choi3@tmax.co.kr>
2022-02-10 21:34:20 +0900

    feat : main 진입점 구현

---
+++ src/test/java/com/spring/Main.java
@@ -0,0 +1,11 @@
+package com.spring;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+    @Test
+    void main(){
+        String[] args = {"3", "1", "2", "4"};
+        Main.main(args);
+    }
+}
